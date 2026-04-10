f01106aa1dea465a10bd2d5df3023b0f7a388bec
Coder <coder@examforge.local>
2026-04-10 00:20:50 +0000

    fix: use absolute path to require server.js

--- frontend/server-custom.js
+++ frontend/server-custom.js
@@ -1,3 +1,4 @@
 // Load URL decode patch BEFORE server.js loads
-require('./server-patch.js');
-require('./server.js');
+const path = require('path');
+require(path.join(__dirname, 'server-patch.js'));
+require(path.join(__dirname, 'server.js'));
